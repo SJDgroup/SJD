1ef04ca5389eaaf5a3cf94ba87e87460bd392130
Carlo Colantuoni <colantuonicarlo@gmail.com>
2026-09-02 02:37:14 -0400

    Update getMatch.R

--- R/getMatch.R
+++ R/getMatch.R
@@ -29,7 +29,10 @@
 #' @export
 
 getMatch <- function(genes, inSpecies, inType, newSpecies, useNewestVersion = FALSE, moreAttrIn = NA, moreAttrNew = NA, mirror = NA, forceINPUTorder1to1=TRUE){
-    species = data.frame(
+
+	cat("begin!")
+
+	species = data.frame(
         species.nm = c("human", "mouse", "roundworm",
                        "fruitfly", "zebrafish", "frog", "chicken", "rat", "guinea pig",
                        "golden hamster", "rabbit", "pig", "sheep", "cow", "dog",
@@ -39,29 +42,8 @@ getMatch <- function(genes, inSpecies, inType, newSpecies, useNewestVersion = FA
                           "danio rerio", "xenopus tropicalis", "gallus gallus", "ratus norvegicus", "cavia porcellus",
                           "melanochromis auratus", "oryctolagus cuniculus", "sus scrofa domesticus",
                           "ovis aries", "bos taurus", "canis lupus familiaris",
-                          "felis catus", "macaca mulatta", "pan paniscus", "pan troglodytes","Mustela putorius furo"),
-        # ensembl.nms = c("hsapiens_gene_ensembl", "mmusculus_gene_ensembl",
-                        # "celegans_gene_ensembl", "dmelanogaster_gene_ensembl",
-                        # "drerio_gene_ensembl", "xtropicalis_gene_ensembl", "ggallus_gene_ensembl", "rnorvegicus_gene_ensembl",
-                        # "cporcellus_gene_ensembl", "mauratus_gene_ensembl",
-                        # "ocuniculus_gene_ensembl", "sscrofa_gene_ensembl",
-                        # "oaries_gene_ensembl", "btaurus_gene_ensembl", "clfamiliaris_gene_ensembl",
-                        # "fcatus_gene_ensembl", "mmulatta_gene_ensembl", "ppaniscus_gene_ensembl",
-                        # "ptroglodytes_gene_ensembl","mpfuro_gene_ensembl"),
- #       genesymbol.attr = c("hgnc_symbol",
- #                           "mgi_symbol", "external_gene_name", "external_gene_name",
- #                           "hgnc_symbol", "hgnc_symbol", "hgnc_symbol", "external_gene_name",
- #                           "hgnc_symbol", "mgi_symbol", "hgnc_symbol", "hgnc_symbol",
- #                           "hgnc_symbol", "hgnc_symbol", "hgnc_symbol", "hgnc_symbol",
- #                           "hgnc_symbol", "hgnc_symbol", "hgnc_symbol"), row.names = 1)
-         genesymbol.attr = c("external_gene_name",
-                            "external_gene_name", "external_gene_name", "external_gene_name",
-                            "external_gene_name", "external_gene_name", "external_gene_name", "external_gene_name",
-                            "external_gene_name", "external_gene_name", "external_gene_name", "external_gene_name",
-                            "external_gene_name", "external_gene_name", "external_gene_name", "external_gene_name",
-                            "external_gene_name", "external_gene_name", "external_gene_name","external_gene_name"), row.names = 1)
-    # mart.in = species[inSpecies, ]$ensembl.nms
-    # mart.new = species[newSpecies, ]$ensembl.nms
+                          "felis catus", "macaca mulatta", "pan paniscus", "pan troglodytes","Mustela putorius furo")
+		, row.names = 1)
 
     # if(useNewestVersion){
         # if(is.na(mirror)){df.in = useMart(biomart = "ensembl", dataset = mart.in)}
@@ -72,34 +54,22 @@ getMatch <- function(genes, inSpecies, inType, newSpecies, useNewestVersion = FA
         # if(is.na(mirror)){df.in = useMart(biomart = "ensembl", dataset = mart.in, host = "https://jun2026.archive.ensembl.org")}
         # if(is.na(mirror)){df.new = useMart(biomart = "ensembl", dataset = mart.new, host = "https://jun2026.archive.ensembl.org")}
     # }
+		
+    if (inType == "symbol") {filter = "external_gene_name"}
+    else if (inType == "ensembl") {filter = "ensembl_gene_id"}
 	
-    symbol.in = as.character(species[inSpecies, "genesymbol.attr"])
-    symbol.new = as.character(species[newSpecies, "genesymbol.attr"])
-    if (inType == "symbol") {
-        filter = as.character(species[inSpecies, "genesymbol.attr"])
-    }
-    else if (inType == "ensembl") {
-        filter = "ensembl_gene_id"
-    }
     cat("You have input ", length(genes), " genes\n")
-    atb.in = c(symbol.in, "ensembl_gene_id", "description")
-    atb.new = c(symbol.new, "ensembl_gene_id", "description")
-    if (!is.na(moreAttrIn)) {
-        atb.in = c(atb.in, moreAttrIn)
-    }
-    if (!is.na(moreAttrNew)) {
-        atb.new = c(atb.new, moreAttrNew)
-    }
-    #tbl.match = getLDS(attributes = atb.in, mart = df.in, filters = filter,
-    #                   values = genes, martL = df.new, attributesL = atb.new)
+    atb.in = c("external_gene_name", "ensembl_gene_id", "description")
+    atb.new = c("external_gene_name", "ensembl_gene_id", "description")
 	
-    if(inSpecies!=newSpecies){tbl.match = getLDS(attributes = atb.in, species = inSpecies, filters = filter,
-                       values = genes, speciesL = newSpecies, attributesL = atb.new)}
-	
-    if(inSpecies==newSpecies){tbl.match = getBM(attributes = atb.in, species = inSpecies, filters = filter,
-                       values = genes)}
+    if (!is.na(moreAttrIn)) {atb.in = c(atb.in, moreAttrIn)}
+    if (!is.na(moreAttrNew)) {atb.new = c(atb.new, moreAttrNew)}
+    	
+    if(inSpecies!=newSpecies){tbl.match = getLDS(attributes = atb.in, species = inSpecies, filters = filter, values = genes, speciesL = newSpecies, attributesL = atb.new)}
+	if(inSpecies==newSpecies){tbl.match = getBM(attributes = atb.in, species = inSpecies, filters = filter, values = genes)}
 
 	cat("hola00")
+	
     if(forceINPUTorder1to1){
 		if(inType=="symbol"){indx=match(genes,tbl.match[,"external_gene_name"])}
 		if(inType=="ensembl"){indx=match(genes,tbl.match[,"ensembl_gene_id"])}
@@ -108,15 +78,13 @@ getMatch <- function(genes, inSpecies, inType, newSpecies, useNewestVersion = FA
 	}
 	if(!forceINPUTorder1to1){cat("not yet implemented - this could be useful for many to one mappings such as fish to mammal.")}
 	
-    tbl.match[, "Gene.description"] = gsub(" \\[.*", "", tbl.match[,
-                                                                   "Gene.description"])
-    tbl.match[, "Gene.description.1"] = gsub(" \\[.*", "", tbl.match[,
-                                                                     "Gene.description.1"])
-    colnames(tbl.match)[1:length(atb.in)] = paste0(colnames(tbl.match)[1:length(atb.in)],
-                                                   ".", inSpecies)
+    tbl.match[, "Gene.description"] = gsub(" \\[.*", "", tbl.match[, "Gene.description"])
+    tbl.match[, "Gene.description.1"] = gsub(" \\[.*", "", tbl.match[, "Gene.description.1"])
+    colnames(tbl.match)[1:length(atb.in)] = paste0(colnames(tbl.match)[1:length(atb.in)], ".", inSpecies)
     colnames(tbl.match)[(length(atb.in) + 1):(length(atb.in) +
                                                   length(atb.new))] = paste0(colnames(tbl.match)[(length(atb.in) +
                                                                                                       1):(length(atb.in) + length(atb.new))], ".", newSpecies)
+	
     cat("We found ", dim(tbl.match)[1], " matches\n")
     cat(sum(duplicated(tbl.match[, 1])), " of those are duplicates and only keeping the 1st of each\n")
     if (inType == "symbol") {
@@ -129,5 +97,6 @@ getMatch <- function(genes, inSpecies, inType, newSpecies, useNewestVersion = FA
         rownames(tbl.match) = NULL
         tbl.return = cbind(genes, tbl.match, stringsAsFactors = FALSE)
     }
+	
     return(tbl.return)
 }
