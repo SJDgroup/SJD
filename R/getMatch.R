6c3069242bd5b44a148e57d89c743e65e2bb8597
Carlo Colantuoni <colantuonicarlo@gmail.com>
2026-09-01 23:50:04 -0400

    Update getMatch.R to use remart

--- R/getMatch.R
+++ R/getMatch.R
@@ -1,6 +1,6 @@
 #' Species matching function
 #'
-#' Use biomaRt to retrieve additional gene identifiers for a vector of gene identifiers in one species (e.g. 'human'), or gene identifiers for orthologous genes in a second species (e.g. 'mouse')
+#' Use remart (originally used biomaRt) to retrieve additional gene identifiers for a vector of gene identifiers in one species (e.g. 'human'), or gene identifiers for orthologous genes in a second species (e.g. 'mouse')
 #'
 #' @param genes character vector of gene identifiers in either ensembl gene identifier or gene symbol format
 #' @param inSpecies a character vector indicating the species from which 'genes' come; must be one of: "human", "mouse", "roundworm", "fruitfly", "zebrafish", "chicken", "rat", "guinea pig", "golden hamster", "rabbit","pig", "sheep", "cow", "dog", "cat", "macaque", "bonobo", "chimpanzee"
@@ -11,7 +11,7 @@
 #' @param useNewestVersion logical indicating if the function should attempt to use the latest version of ensembl, or to use the May 2025 archive version that is more stable. default is FALSE.
 #' @param mirror a single character value indicating the alternate Ensembl mirror site to use, e.g. "https://useast.ensembl.org" or "https://asia.ensembl.org". This is useful if the primary Ensembl server is down or returning errors. This argument is only used if useNewestVersion=TRUE.
 #'
-#' @importFrom biomaRt useMart getLDS
+#' @importFrom remart useMart getLDS
 #'
 #' @return A matrix whose first column contains the exact gene identifiers (and in the same order) that were sent to the function in the "genes" argument, followed by 3 columns of gene identifiers that were retrieved from biomaRt from both the "inSpecies" and the "newSpecies" (for each species, these 3 identifiers are: gene symbol, ensembl gene ID, and text description).
 #'
