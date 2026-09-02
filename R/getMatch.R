2dac8188489a7ee28b63d84dbb4834a9796be6e2
Carlo Colantuoni <colantuonicarlo@gmail.com>
2026-09-02 03:04:15 -0400

    Update getMatch.R

--- R/getMatch.R
+++ R/getMatch.R
@@ -72,12 +72,11 @@ getMatch <- function(genes, inSpecies, inType, newSpecies, useNewestVersion = FA
 		tbl.match=tbl.match[indx,]
 	}
 	if(!forceINPUTorder1to1){cat("not yet implemented - this could be useful for many to one mappings such as fish to mammal.")}
-cat("01")
-cat(str(tbl.match))
-	tbl.match[, "Gene.description"] = gsub(" \\[.*", "", tbl.match[, "Gene.description"])
-    tbl.match[, "Gene.description.1"] = gsub(" \\[.*", "", tbl.match[, "Gene.description.1"])
+
+	tbl.match[, "description"] = gsub(" \\[.*", "", tbl.match[, "Gene.description"])
 cat("02")
     if(inSpecies!=newSpecies){
+		tbl.match[, "description.1"] = gsub(" \\[.*", "", tbl.match[, "Gene.description.1"])
 		colnames(tbl.match)[1:length(atb.in)] = paste0(colnames(tbl.match)[1:length(atb.in)], ".", inSpecies)
     	colnames(tbl.match)[(length(atb.in) + 1):(length(atb.in) + length(atb.new))] = paste0(colnames(tbl.match)[(length(atb.in) + 1):(length(atb.in) + length(atb.new))], ".", newSpecies)
 	}
